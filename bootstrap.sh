7eea441db237ee7cf9588b28b91aecb6b5e7f6f8
Nikhil Ketkar <nikhil.ketkar@flipkarts-MacBook-Pro.local>
2016-01-10 15:58:51 +0530

    Caffe Dependencies Completed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 apt-get update
+
+apt-get install -y cmake
+apt-get install -y emacs
+apt-get install -y git
+apt-get install -y ipython
+apt-get install -y libatlas-base-dev
 apt-get install -y libboost-all-dev
 apt-get install -y libgflags-dev
 apt-get install -y libgoogle-glog-dev
@@ -12,10 +18,10 @@ apt-get install -y libprotobuf-dev
 apt-get install -y libsnappy-dev
 apt-get install -y protobuf-compiler
 apt-get install -y python-numpy
-apt-get install -y git
-apt-get install -y emacs
-apt-get install -y cmake
-apt-get install -y libatlas-base-dev
+apt-get install -y python-protobuf
+apt-get install -y python-scikits-learn
+apt-get install -y python-skimage
+
 
 
 
