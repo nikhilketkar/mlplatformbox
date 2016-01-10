25c40c50469830bf7260c439bcaa0c824abef530
Nikhil Ketkar <nikhil.ketkar@flipkarts-MacBook-Pro.local>
2016-01-10 15:24:41 +0530

    Initial Commit

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y libboost-all-dev
+apt-get install -y libgflags-dev
+apt-get install -y libgoogle-glog-dev
+apt-get install -y libhdf5-serial-dev
+apt-get install -y libleveldb-dev
+apt-get install -y liblmdb-dev 
+apt-get install -y libopencv-dev
+apt-get install -y libprotobuf-dev
+apt-get install -y libsnappy-dev
+apt-get install -y protobuf-compiler
+apt-get install -y python-numpy
+apt-get install -y git
+apt-get install -y emacs
+apt-get install -y cmake
+apt-get install -y libatlas-base-dev
+
+
+
