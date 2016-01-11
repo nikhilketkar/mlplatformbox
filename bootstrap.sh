8543b48489bb4df61067387fa287cc813b59dd0f
Nikhil Ketkar <nikhil.ketkar@flipkarts-MacBook-Pro.local>
2016-01-11 22:37:23 +0530

    ..

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,6 +26,8 @@ apt-get install -y screen
 apt-get install -y python-requests
 apt-get install -y python-opencv
 apt-get install -y python-setuptools
+apt-get install -y python-falcon
+apt-get install -y gunicorn
 
 
 
