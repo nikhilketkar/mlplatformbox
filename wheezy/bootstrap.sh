b712b401f50c02d0a459bd57db43e496e3c9a126
Nikhil Ketkar <nikhil.ketkar@flipkarts-MacBook-Pro.local>
2016-01-12 13:45:35 +0530

    ..

--- wheezy/bootstrap.sh
+++ wheezy/bootstrap.sh
@@ -9,10 +9,8 @@ apt-get install -y ipython
 apt-get install -y libatlas-base-dev
 apt-get install -y libboost-all-dev
 apt-get install -y libgflags-dev
-apt-get install -y libgoogle-glog-dev
 apt-get install -y libhdf5-serial-dev
 apt-get install -y libleveldb-dev
-apt-get install -y liblmdb-dev 
 apt-get install -y libopencv-dev
 apt-get install -y libprotobuf-dev
 apt-get install -y libsnappy-dev
@@ -21,13 +19,24 @@ apt-get install -y python-numpy
 apt-get install -y python-protobuf
 apt-get install -y python-scikits-learn
 apt-get install -y python-skimage
-apt-get install -y fabric
 apt-get install -y screen
 apt-get install -y python-requests
 apt-get install -y python-opencv
 apt-get install -y python-setuptools
-apt-get install -y python-falcon
 apt-get install -y gunicorn
+apt-get install -y automake
+apt-get install -y libssl-dev
+apt-get install -y checkinstall
+apt-get install -y byacc
+apt-get install -y flex
+apt-get install -y zip
+apt-get install -y unzip
 
-
+# apt-get install -y python-falcon
+# apt-get install -y fabric
+# apt-get install -y liblmdb-dev 
+# apt-get install -y libgoogle-glog-dev
+# apt-get install -y thrift-compiler
+# apt-get install -y python-thrift
+#apt-get install -y cython
 
