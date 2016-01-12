0ab48f90c024d0914d60d0b212d831dbb1ce3f15
Nikhil Ketkar <nikhil.ketkar@flipkarts-MacBook-Pro.local>
2016-01-12 10:30:04 +0530

    Seperate boxes for Wheezy and Jessie

---
+++ wheezy/bootstrap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+apt-get update
+
+apt-get install -y cmake
+apt-get install -y emacs
+apt-get install -y git
+apt-get install -y ipython
+apt-get install -y libatlas-base-dev
+apt-get install -y libboost-all-dev
+apt-get install -y libgflags-dev
+apt-get install -y libgoogle-glog-dev
+apt-get install -y libhdf5-serial-dev
+apt-get install -y libleveldb-dev
+apt-get install -y liblmdb-dev 
+apt-get install -y libopencv-dev
+apt-get install -y libprotobuf-dev
+apt-get install -y libsnappy-dev
+apt-get install -y protobuf-compiler
+apt-get install -y python-numpy
+apt-get install -y python-protobuf
+apt-get install -y python-scikits-learn
+apt-get install -y python-skimage
+apt-get install -y fabric
+apt-get install -y screen
+apt-get install -y python-requests
+apt-get install -y python-opencv
+apt-get install -y python-setuptools
+apt-get install -y python-falcon
+apt-get install -y gunicorn
+
+
+
