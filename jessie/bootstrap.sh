3e41aac499ae8cfcad25fa3f1229d510b2d40088
Nikhil Ketkar <nikhil.ketkar@flipkarts-MacBook-Pro.local>
2016-01-13 20:37:53 +0530

    ..

--- jessie/bootstrap.sh
+++ jessie/bootstrap.sh
@@ -2,9 +2,17 @@
 
 apt-get update
 
+apt-get install -y automake
+apt-get install -y build-essential
+apt-get install -y byacc
+apt-get install -y checkinstall
 apt-get install -y cmake
-apt-get install -y emacs
+apt-get install -y cython
+apt-get install -y emacs24
+apt-get install -y fabric
+apt-get install -y flex
 apt-get install -y git
+apt-get install -y gunicorn
 apt-get install -y ipython
 apt-get install -y libatlas-base-dev
 apt-get install -y libboost-all-dev
@@ -13,21 +21,25 @@ apt-get install -y libgoogle-glog-dev
 apt-get install -y libhdf5-serial-dev
 apt-get install -y libleveldb-dev
 apt-get install -y liblmdb-dev 
+apt-get install -y liblmdb-dev 
 apt-get install -y libopencv-dev
 apt-get install -y libprotobuf-dev
 apt-get install -y libsnappy-dev
+apt-get install -y libssl-dev
+apt-get install -y libtool
+apt-get install -y pkg-config
 apt-get install -y protobuf-compiler
-apt-get install -y python-numpy
-apt-get install -y python-protobuf
-apt-get install -y python-scikits-learn
-apt-get install -y python-skimage
-apt-get install -y fabric
-apt-get install -y screen
-apt-get install -y python-requests
-apt-get install -y python-opencv
-apt-get install -y python-setuptools
 apt-get install -y python-falcon
-apt-get install -y gunicorn
-
-
+apt-get install -y python-numpy
+apt-get install -y python-opencv
+apt-get install -y python-protobuf
+apt-get install -y python-requests
+apt-get install -y python-scikits-learn
+apt-get install -y python-setuptools
+apt-get install -y python-skimage
+apt-get install -y python-thrift
+apt-get install -y screen
+apt-get install -y thrift-compiler
+apt-get install -y unzip
+apt-get install -y zip
 
